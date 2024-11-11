8de5080d0dfbd59279bd4482a8160c05d67c0118
githublees <leeho_@naver.com>
2024-11-11 18:47:11 +0900

    style(application): Application 메인 의존성 코드 정리

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,16 +1,11 @@
 package store;
 
 import store.controller.ConvenienceManager;
-import store.domain.Products;
-import store.domain.Promotions;
-import store.view.InputView;
-import store.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        ConvenienceManager convenienceManager = new ConvenienceManager(
-                new Products(), new Promotions(), new InputView(), new OutputView());
+        ConvenienceManager convenienceManager = new ConvenienceManager();
         convenienceManager.run();
     }
 }
