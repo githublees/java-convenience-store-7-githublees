cdb68259b9580d5de303c4984819fa9222d4d6a4
githublees <leeho_@naver.com>
2024-11-11 18:43:51 +0900

    test(controller): 편의점 프로그램 컨트롤러 작성 전 테스트 케이스 작성

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -54,9 +54,65 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 프로모션_외_상품_구매_테스트() {
+        assertSimpleTest(() -> {
+            run("[콜라-11]", "Y", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈8,000");
+        });
+    }
+
+    @Test
+    void 프로모션_적용된_상품만_구매_테스트() {
+        assertSimpleTest(() -> {
+            run("[콜라-11]", "N", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈6,000");
+        });
+    }
+
+    @Test
+    void 멤버십만_적용_테스트() {
+        assertSimpleTest(() -> {
+            run("[콜라-11]", "N", "Y", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈6,000");
+        });
+    }
+
+    @Test
+    void 프로모션_멤버십_적용_테스트() {
+        assertSimpleTest(() -> {
+            run("[콜라-11]", "Y", "Y", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈7,400");
+        });
+    }
+
+    @Test
+    void 잘못된_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("[콜라-3]", "네", "네");
+            assertThat(output()).contains("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 잘못된_형식_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("[[콜라-3]],[에너지바-5]", "N", "N");
+            assertThat(output()).contains("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("[컵라면-12]", "N", "N");
+            assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 예외_테스트_프로모션_적용() {
+        assertSimpleTest(() -> {
+            runException("[컵라면-12]", "Y", "N");
             assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
         });
     }
