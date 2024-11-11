252f761d8ab1000d7f68a2417339e3e473a4d6c9
githublees <leeho_@naver.com>
2024-11-11 18:43:05 +0900

    test(handler): 입력 핸들러 관련 테스트 케이스 추가 작성

--- src/test/java/store/view/InputHandlerTest.java
+++ src/test/java/store/view/InputHandlerTest.java
@@ -6,7 +6,10 @@ import static org.assertj.core.api.Assertions.tuple;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import store.dto.StockDTO;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import store.dto.BuyDTO;
+import store.exception.ErrorMessage;
 
 class InputHandlerTest {
 
@@ -14,19 +17,19 @@ class InputHandlerTest {
     void 구매할_상품명과_수량_DTO_반환_테스트() {
         String input = "[사이다-2],[감자칩-1]";
 
-        List<StockDTO> items = InputHandler.splitItems(input);
+        List<BuyDTO> items = InputHandler.splitItems(input);
 
         assertThat(items).extracting("name", "quantity")
                 .contains(tuple("사이다", 2),
                           tuple("감자칩", 1));
     }
 
-    @Test
-    void 구매할_수량이_양수가_아닌_경우_예외_테스트() {
-        String input = "[사이다-1],[감자칩--1]";
+    @ParameterizedTest
+    @ValueSource(strings = {"[사이다-1],[감자칩--1]", "[[콜라-3]],[에너지바-5]", "[사이다-1],[콜라-a]"})
+    void 올바른_형식이_아닌_경우_예외_테스트(String input) {
         assertThatThrownBy(() -> InputHandler.splitItems(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 수량은 음수가 아닌 정수여야 합니다.");
+                .hasMessage(ErrorMessage.NOT_FIT_THE_FORM_MESSAGE.toString());
     }
 
 }
